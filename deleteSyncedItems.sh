49db96ad798f68c44d66b086ee64e30a5cf05f12
Jaret Clifton <jclifton@emeralddesign.com>
2020-04-01 08:45:18 -0700

    Update deleteSyncedItems.sh

--- deleteSyncedItems.sh
+++ deleteSyncedItems.sh
@@ -1,1 +1,1 @@
-find "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Cache/Transcode/Sync+" -type d -mtime +1 -exec rm -rf {} +
+find "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Cache/Transcode/Sync+" -type d -mtime +2 -exec rm -rf {} +
