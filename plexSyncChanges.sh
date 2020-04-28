e48ed12eefbf6a2e5ea8570acae2840c4eb5f772
Jaret Clifton <jclifton@emeralddesign.com>
2020-04-28 11:16:30 -0700

    Update plexSyncChanges.sh

--- plexSyncChanges.sh
+++ plexSyncChanges.sh
@@ -4,6 +4,7 @@ plexToken="[INSERT PLEX TOKEN HERE]"
 
 #Check the status of the primary Plex server by looking for "<MediaContainer" in the output of the curl command
 plexPrimaryStatus=`curl https://[INSERT LOCAL PLEX HOST HERE]:32400/status/sessions?X-Plex-Token=$plexToken -s -k | grep "<MediaContainer" | wc -l`
+rsyncStatus=`ps -ef | grep rsync | grep -v grep | grep plex | wc -l`
 
 if [ $plexPrimaryStatus -eq 0 ]
         then
@@ -12,6 +13,14 @@ fi
 
 if [ $plexPrimaryStatus -eq 1 ]
         then
-                echo `date` "[PLEX OK] Plex is up and responding on the primary node. Syncing changes to the secondary."
-                nice -n19 ionice -c2 -n7 sshpass -p "[INSERT SSH PASSWORD HERE]" rsync -uav --delete-during --progress --exclude='Library/Application Support/Plex Media Server/Cache/Transcode/Sync+' /var/lib/plexmediaserver/* [INSERT SECONDARY PLEX HOST HERE]:/var/lib/plexmediaserver
+                if [ $rsyncStatus -gt 3 ]
+                        then
+                                echo `date` "[RSYNC ERROR] Multiple {$rsyncStatus} concurrent rsync jobs have been discovered. Aborting current transfer."
+                        fi
+                if [ $rsyncStatus -eq 0 ]
+                        then
+                        echo `date` "[PLEX OK] Plex is up and responding on the primary node. Syncing changes to the secondary."
+                        echo `date` "[RSYNC STATUS] $rsyncStatus jobs have been discovered."
+                        nice -n 19 ionice -c 2 -n 7 sshpass -p "[INSERT SSH PASSWORD HERE]" rsync -uav --delete-during --exclude='Library/Application Support/Plex Media Server/Cache/Transcode/Sync+' /var/lib/plexmediaserver/* [INSERT REMOTE HOST HERE]:/var/lib/plexmediaserver
                 fi
+fi
