08473d732d54e460150b2d1ba7c46902278b5681
Jaret Clifton <jclifton@emeralddesign.com>
2023-08-09 17:56:36 -0700

    Update plexStatusCheck.sh

--- Plex/plexStatusCheck.sh
+++ Plex/plexStatusCheck.sh
@@ -1,62 +1,51 @@
 #!/bin/sh
 
-logFile="/opt/plexStatusCheck.log"
-logData=$(cat "$logFile")
-plexToken="[INSERT PLEX TOKEN HERE]"
-plexRunning=`systemctl show -p SubState --value plexmediaserver`
+while true
+do
 
-#Check the status of the primary Plex server by looking for "<MediaContainer" in the output of the curl command
-plexPrimaryStatus=`curl https://ct-plex.emeralddesign.com:32400/status/sessions?X-Plex-Token=$plexToken -s -k | grep "<MediaContainer" | wc -l`
+        logFile="/opt/logs/plexStatusCheck.log"
+        plexToken="[INSERT PLEX TOKEN HERE]"
+        plexRunning=`systemctl show -p SubState --value plexmediaserver`
+        slackWebHook="https://hooks.slack.com/services/[INSERT SLACK WEBHOOK TOKEN HERE]"
+        hostName=`hostname`
+        #Check the status of the primary Plex server by looking for "<MediaContainer" in the output of the curl command
+        plexPrimaryStatus=`curl --retry 2 --retry-all-errors --connect-timeout 3 http://[INSERT LOCAL PRIMARY PLEX SERVER URL HERE]:32400/identity -s | grep "<MediaContainer" | wc -l`
 
-#Restart the local Plex server to pickup database changes sync'd from the primary if the curl fails to return a count of 1
-if [ $plexPrimaryStatus -eq 0 ]
-        then
-                if [ "$logData" =  "Secondary Restarted" ]
-                        then
-                                echo `date`
-                                echo "[PLEX FAILOVER] Plex already restarted on the secondary instance. No action to take."
-                fi
-fi
+        #Restart the local Plex server to pickup database changes sync'd from the primary if the curl fails to return a count of 1
+        if [ $plexPrimaryStatus -eq 0 ]
+                then
+                        if [ "$plexRunning" = "running" ]
+                                then
+                                        echo `date` "[PLEX FAILOVER] Plex already restarted on $hostName. No action to take." | tee -a $logFile
+                        fi
+                        if [ "$plexRunning" != "running" ]
+                                then
+                                        echo `date` "[PLEX CRITICAL] Plex is down on the primary and secondary nodes. Restarting Plex on $hostName to pickup database changes." | tee -a $logFile
+                                        systemctl restart plexmediaserver.service
+                                        echo `date` "[PLEX RESTARTED] Plex has been restarted on $hostName." | tee -a $logFile
+                                        plexRunningRestart=`systemctl show -p SubState --value plexmediaserver`
+                                                if [ "$plexRunningRestart" = "running" ]
+                                                        then
+                                                                curl -s -X POST -H 'Content-type: application/json' --data "{\"text\":\"Plex Server Failover Event! $hostName is now active.\"}" $slackWebHook
+                                                fi
+                        fi
 
-if [ $plexPrimaryStatus -eq 0 ]
-        then
-                if [ "$logData" = "" ]
-                        then
-                                echo `date` 
-                                echo "[PLEX CRITICAL] Plex is down on the primary instance. Restarting Plex on the secondary to pickup database changes."
-                                systemctl restart plexmediaserver.service
-                                echo "Secondary Restarted" > $logFile
-                                curl -X POST -H 'Content-type: application/json' --data '{"text":"[PLEX FAILOVER] Plex Server Failover Event!"}' https://hooks.slack.com/services/[INSERT TOKEN HERE]
-                fi
-fi
+        fi
 
-if [ $plexPrimaryStatus -eq 1 ]
-        then
-                if [ "$logData" = "Secondary Restarted" ]
-                        then
-                                echo `date`
-                                echo "[PLEX FAILOVER OK] Plex is now available on the primary node once again. Secondary node Plex service stopped."
-                                curl -X POST -H 'Content-type: application/json' --data '{"text":"[PLEX FAILOVER OK] Plex Server Primary OK!"}' https://hooks.slack.com/services/[INSERT TOKEN HERE]
-                                `cat /dev/null` > $logFile
-                                        if [ "$plexRunning" = "running" ]
-                                                then
-                                                        systemctl stop plexmediaserver.service
-                                                        echo "Stopped local Plex instance."
-                                        fi
-                fi
-fi
+        if [ $plexPrimaryStatus -eq 1 ]
+                then
+                        if [ "$plexRunning" = "running" ]
+                                then
+                                        echo `date` "[PLEX FAILOVER OK] Plex is now available on the primary node once again. Shutting down Plex on $hostName" | tee -a $logFile
+                                        curl -s -X POST -H 'Content-type: application/json' --data '{"text":"Plex Server Primary OK!"}' $slackWebHook
+                                        systemctl stop plexmediaserver.service
+                        fi
 
-if [ $plexPrimaryStatus -eq 1 ]
-        then
-                if [ "$logData" = "" ]
-                        then
-                                echo `date` 
-                                echo "[PLEX OK] Plex up and responding on the primary. No action to take."
-                        `cat /dev/null` > $logFile
-                                        if [ "$plexRunning" = "running" ]
-                                                then
-                                                        systemctl stop plexmediaserver.service
-                                                        echo "Stopped local Plex instance."
-                                        fi
-                fi
-fi
+                        if [ "$plexRunning" != "running" ]
+                                then
+                                        echo `date` "[PLEX OK] Plex up and responding on the primary. No action to take." | tee -a $logFile
+                        fi
+        fi
+
+sleep 5
+done
